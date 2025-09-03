6c0737b24e7bea7c8cc1eae830bf0a63f6c1e39e
Al Izhar Iqrami <103298330+al-izhariqrami@users.noreply.github.com>
2025-09-03 14:56:18 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,13 +3,14 @@ import numpy as np
 import pandas as pd
 from io import BytesIO
 # import pdfplumber
-import re
-import tabula  # pip install tabula-py
+# import tabula  # pip install tabula-py
 import plotly.express as px
+import re
 
 
 st.title("Preprocessing & Analytic Data RKA SKPD")
 pdf_file = st.file_uploader("Upload file PDF", type="pdf")
+
 
 
 
